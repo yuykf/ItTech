e766af2fbeb136a74c2eb466853ada145ef261b6
yunmai <yuyang@yuntutv.net>
2020-06-22 19:25:07 +0800

    Initial commit

---
+++ app/src/main/java/cn/fungo/ittech/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.fungo.ittech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
